7fa3ab32160a51b3cd9cf40c75566cece26248e7
florchial <mf.chialvo@gmail.com>
2025-08-13 19:32:18 +0200

    WIP: player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 const Pause = () => (<svg width="24"  height="24"  viewBox="0 0 24 24"  fill="#000000"  class="icon icon-tabler icons-tabler-filled icon-tabler-player-pause"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" /><path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" /></svg>)
 const Play = () => (<svg width="24"  height="24"  viewBox="0 0 24 24"  fill="#000000"  class="icon icon-tabler icons-tabler-filled icon-tabler-player-play"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" /></svg>)
 
@@ -6,13 +6,16 @@ const Play = () => (<svg width="24"  height="24"  viewBox="0 0 24 24"  fill="#00
 export function Player() {
     const [isPlaying, setIsPlaying] = useState(false);
     const audioRef = useRef(null);
-    //const [currentSong, setCurrentSong] = useState(null);
+    const [currentSong, setCurrentSong] = useState(null);
+
+    useEffect(() => {
+        audioRef.current.src = `/music/1/01.mp3`;
+    }, []); 
 
     const togglePlay = () => {
         if (isPlaying) {
             audioRef.current.pause();
         } else {
-            audioRef.current.src = `/music/1/01.mp3`;
             audioRef.current.play();
         }
         setIsPlaying(!isPlaying);
