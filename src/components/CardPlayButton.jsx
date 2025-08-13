25d05c601af1b676322e0714dd731b19db95dbd5
florchial <mf.chialvo@gmail.com>
2025-08-13 20:43:15 +0200

    play button for playlist card

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,7 @@
+import { Play, Pause } from "./Player"
+export function CardPlayButton() {
+    return (
+    <div className="card-play-button rounded-full bg-green-500 p-3">
+        <Play />
+    </div>)
+}
