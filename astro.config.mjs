6ef7bde0d829e9a48c0a96b36aa161302a733017
florchial <mf.chialvo@gmail.com>
2025-08-13 16:42:31 +0200

    adding svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   output: 'server',
+  integrations: [svelte()],
 });
