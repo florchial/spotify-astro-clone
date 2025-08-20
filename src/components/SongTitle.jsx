5c5530176e6e20629adc3056da9f368dc51127e5
florchial <mf.chialvo@gmail.com>
2025-08-20 12:20:22 +0200

    songTitle component

---
+++ src/components/SongTitle.jsx
@@ -0,0 +1,34 @@
+import { usePlayerStore } from "@/store/playerStore"
+
+export function SongTitle() {
+    const { currentState } = usePlayerStore(state => state)
+    const { song } = currentState
+
+    return (
+        song ?
+            <div className="flex items-center gap-4 relative overflow-hidden">
+
+                <picture className="w-16 h-16 bg-zinc-800 rounded-lg shadow-lg">
+                    <img className="rounded-lg object-cover w-full h-full" src={song.coverUrl} alt={song.title} />
+                </picture>
+                <div>
+                    <h3 className="block">
+                        {song.title}
+                    </h3>
+                    <p className="text-sm text-zinc-400">
+                        {song.artist}
+                    </p>
+                </div>
+
+            </div>
+            : <div className="flex items-center gap-5 relative overflow-hidden">
+                <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
+                    <img src="/images/placeholder-zinc.svg" alt="No song playing" />
+                </picture>
+                <h3 className="block">
+                    No song playing
+                </h3>
+            </div>
+    )
+
+}
