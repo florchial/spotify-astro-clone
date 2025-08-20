c233826b25a1160b75bb5c0d3bd418a2b5ebfc0e
florchial <mf.chialvo@gmail.com>
2025-08-20 12:22:38 +0200

    add songTitle and volume slider to player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,7 @@
-import { useState, useRef, useEffect } from 'react'
+import { useRef, useEffect } from 'react'
 import { usePlayerStore } from "@/store/playerStore"
+import { SongTitle } from './SongTitle'
+import { Slider } from './Slider'
 export const Pause = () => (<svg width="24"  height="24"  viewBox="0 0 24 24"  fill="#000000"  class="icon icon-tabler icons-tabler-filled icon-tabler-player-pause"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" /><path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" /></svg>)
 export const Play = () => (<svg width="24"  height="24"  viewBox="0 0 24 24"  fill="#000000"  class="icon icon-tabler icons-tabler-filled icon-tabler-player-play"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" /></svg>)
 
@@ -12,31 +14,39 @@ export function Player() {
         isPlaying ? audioRef.current.play() : audioRef.current.pause()
     }, [isPlaying]);
 
-
     useEffect(() => {
         const { playlist, songs, song } = currentState
+        if (!song) return
         audioRef.current.src = `/music/${song?.albumId}/${song?.id}.mp3`
         audioRef.current.play()
     }, [currentState]); 
 
     const togglePlay = () => {
+        const { song } = currentState
+        if (!song) return
         setIsPlaying(!isPlaying)
     };
 
     return (
-        <div className="flex flex-row justify-between px-4 w-full z-10">
+        <div className="flex flex-row px-4 w-full z-10">
             <div>
-                Song Title
+                <SongTitle />
             </div>
             <div className="grid place-content-center flex-1">
                 <button className="rounded-full p-2 bg-white" onClick={togglePlay}>
                     {isPlaying ? <Pause /> : <Play />}
                 </button>
             </div>
-            <div>
-                Volume
+            <div className="grid place-content-center">
+                <Slider
+                defaultValue={[90]}
+                max={100}
+                min={0}
+                className="w-[130px]"
+                />
             </div>
             <audio ref={audioRef}/>
+            
         </div>
     )
 }
